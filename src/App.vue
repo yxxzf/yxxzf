ee85ec52203bc62c26439374a487814dc1e40e3c
yhtzf <figezhou334@gmail.com>
2025-06-30 16:11:56 +0800

    添加element-plus，设置src文件夹别名

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
     <h1>我是APP</h1>
+    <el-button type="primary" :icon="Edit">主要按钮</el-button>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import {Edit} from '@element-plus/icons-vue'
+</script>
 
 <style scoped></style>
